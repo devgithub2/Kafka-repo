d6408f1a0c4f0b53150247fda3f2451406afae38
Farooque <mohfarooque@deloitte.com>
2024-02-02 11:26:52 +0530

    topic creation

--- kafka_topics.tf
+++ kafka_topics.tf
@@ -15,3 +15,20 @@ resource "confluent_kafka_topic" "orders" {
   }
 }
 
+resource "confluent_kafka_topic" "orders" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  topic_name    = "confluent.kafka"
+  rest_endpoint = "https://pkc-921jm.us-east-2.aws.confluent.cloud:443"
+
+  credentials {
+    key    = var.confluent_key
+    secret = var.confluent_secret
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
